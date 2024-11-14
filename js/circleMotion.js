4065a89a08f23c83e3289a472d5536a9bf4db505
koya-kimura <2ktmnri8@gmail.com>
2024-11-14 16:01:37 +0900

    remove:崩す前

--- js/circleMotion.js
+++ js/circleMotion.js
@@ -1,16 +1,33 @@
 class CircleMotion {
     constructor(){
+        this.grid = min(width, height) / 10;
+        this.vx = 10;;
 
+        this.px = [];
+        this.py = [];
+        for(let x = -this.grid; x < width + this.grid; x += this.grid){
+            for (let y = -this.grid; y < height + this.grid; y += this.grid){
+                this.px.push(x);
+                this.py.push(y);
+            }
+        }
     }
 
     update(beatInfo, colorPalette){
-
+        for(let i = 0; i < this.px.length; i++){
+            this.px[i] +=this.vx;
+            if(this.px[i] > width + this.grid){
+                this.px[i] = -this.grid;
+            }
+        }
     }
 
     display(tex){
-        tex.textAlign(CENTER);
-        tex.fill(255, 0, 50);
-        tex.textSize(100);
-        tex.text("A", width/2, height/2);
+        for(let i = 0; i < this.px.length; i++){
+            for(let j = 0; j < this.py.length; j++){
+                tex.fill(255);
+                tex.circle(this.px[i], this.py[j], this.grid);
+            }
+        }
     }
 }
